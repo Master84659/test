6b30dc94c3660df2798a94c3c78121f00f992cf8
Murad <jakjason1987@gmail.com>
2025-02-17 00:31:38 +0200

    add spannnn

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+
+void main () {
+       
+int x ;
+int p;
+x =p = 44;
+x +=5;
+double y ;
+y = x / p;
+print(p  );
+print(x  );
+print(y );
+print(p < 50 && x < 30);
+print(num.parse("11111"));
+for(x = 1 ; x <= 10 ; x++ )
+{
+    if (x == 8)
+    {
+print("xxxxxxxxx");
+    }
+    print(x);
+}
+
+//print("hasaxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxkkm,jkjlmknkbjn melad murad nada");
+//print("bvgtyb8yhuinxcfujknbgg hghnjbvghhjjknbgh+pkp;,kl;,kk n l;mkkm, hjnhjn");
+
+
+}
